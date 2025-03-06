53676fc9c7bab4252d0335b8d04d54574053ff45
deepan <deepanappu1999@gmail.com>
2025-03-06 18:42:50 +0530

    changing the colors

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -103,26 +103,23 @@ export class DashboardComponent implements OnInit{
           datasets: [
               {
                   type: 'bar',
-                  label: 'Dataset 1',
-                  backgroundColor: ['rgba(87, 96, 111, 0.59)','rgba(87, 96, 111, 0.81)','rgba(87, 96, 111, 0.8)','rgba(87, 96, 111, 0.81)'],
-                  borderColor: ['rgb(255, 159, 64)', 'rgb(75, 192, 192)', 'rgb(54, 162, 235)', 'rgb(153, 102, 255)'],
-                  data: [50, 25, 12, 48, 90, 76, 42],
+                  label: 'All movies',
+                  backgroundColor: ['rgba(87, 96, 111, 0.65)','rgba(87, 96, 111, 0.81)','rgba(87, 96, 111, 0.8)','rgba(87, 96, 111, 0.81)'],
+                 data: [50, 25, 12, 48, 90, 76, 42],
                   borderRadius:10
               },
               {
                   type: 'bar',
-                  label: 'Dataset 2',
-                  backgroundColor: ['rgba(87, 96, 111, 0.59)','rgba(87, 96, 111, 0.72)','rgba(87, 96, 111, 0.73)','rgba(87, 96, 111, 0.62)'],
-                    borderColor: ['rgb(255, 159, 64)', 'rgb(75, 192, 192)', 'rgb(54, 162, 235)', 'rgb(153, 102, 255)'],
-                  // backgroundColor: documentStyle.getPropertyValue('--green-600'),
+                  label: 'Subcription',
+                  backgroundColor: ['rgba(87, 96, 111, 0.39)','rgba(87, 96, 111, 0.72)','rgba(87, 96, 111, 0.73)','rgba(87, 96, 111, 0.62)'],
+                     // backgroundColor: documentStyle.getPropertyValue('--green-600'),
                   data: [21, 84, 24, 75, 37, 65, 34],
                   borderRadius:10
               },
               {
                   type: 'bar',
-                  label: 'Dataset 3',
+                  label: 'Most watched movies',
                   backgroundColor: ['rgba(87, 96, 111, 0.81)','rgba(87, 96, 111, 0.81)','rgba(87, 96, 111, 0.88)','rgba(87, 96, 111, 0.4)'],
-                   borderColor: ['rgb(255, 159, 64)', 'rgb(75, 192, 192)', 'rgb(54, 162, 235)', 'rgb(153, 102, 255)'],
                   // backgroundColor: documentStyle.getPropertyValue('--yellow-500'),
                   data: [41, 52, 24, 74, 23, 21, 32],
                   borderRadius:10
